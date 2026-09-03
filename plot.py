8f4ed1fd9f1f815995b21988b94c17d9f3f761b0
ignasa007 <jasraj.singh.23@ucl.ac.uk>
2026-09-03 10:02:21 +0200

    combined train/test acc plots in one

--- plot.py
+++ plot.py
@@ -16,31 +16,32 @@ dirs = [
     # ("results/cifar10/resnet20/ivon-price/seed=0/2026-08-18-11-57-06", None),
     # AdamW with IVON-paper settings
     ("results/cifar10/resnet20/adamw/seed=0/2026-08-27-21-41-19", "AdamW"),
-    # IV Adam with step-size 2e-3
-    ("results/cifar10/resnet20/ivadam-decoupled/seed=0/2026-08-27-21-41-17", r"IVAdam ($\eta=2\cdot 10^{-3}$)"),
-    # IV Adam with step-size 2e-2
-    ("results/cifar10/resnet20/ivadam-decoupled/seed=0/2026-08-27-23-29-39", r"IVAdam ($\eta=2\cdot 10^{-2}, \delta=2\cdot 10^{-4}$)"),
+    # IVAdam with step-size 2e-3
+    ("results/cifar10/resnet20/ivadam-decoupled/seed=0/2026-08-27-21-41-17", r"IVAdam ($\eta=2\cdot 10^{-3}, \delta=2\cdot 10^{-4}$)"),
+    # # IV Adam with step-size 2e-2
+    # ("results/cifar10/resnet20/ivadam-decoupled/seed=0/2026-08-27-23-29-39", r"IVAdam ($\eta=2\cdot 10^{-2}, \delta=2\cdot 10^{-4}$)"),
     # # IV Adam with step-size 2e-1
     # ("results/cifar10/resnet20/ivadam-decoupled/seed=0/2026-08-27-23-30-31", r"IV Adam with $\eta=0.2$"),
+    # Adam with AdamW settings
+    ("results/cifar10/resnet20/adam/seed=0/2026-09-02-17-15-51", "Adam"),
+    # IVAdam without sampling
+    ("results/cifar10/resnet20/ivadam-decoupled-atmean/seed=0/2026-09-02-16-19-40", "IVAdam@mean"),
 ]
-split = "test"
 col = "acc"
 
+fig, axs = plt.subplots(1, 2, figsize=(7.2*2, 4.8))
 
-fig, axs = plt.subplots(1, 1, figsize=(6.4,4.8))
+for ax, split in zip(axs, ("train", "test")):
+    for d, name in dirs:
+        fn = f"{d}/{split}.csv"
+        df = pd.read_csv(fn, header=0, index_col=False)
+        label = name if name is not None else basename(d)
+        ax.plot(df.loc[:, "epoch"], df.loc[:, col], linewidth=3, label=label)
+    ax.tick_params(axis="both", which="major", labelsize=12)
+    ax.set_xlabel("Epoch", fontsize=16)
+    ax.grid()
 
-for d, name in dirs:
-    fn = f"{d}/{split}.csv"
-    df = pd.read_csv(fn, header=0, index_col=False)
-    label = name if name is not None else basename(d)
-    axs.plot(df.loc[:, "epoch"], df.loc[:, col], linewidth=3, label=label)
-
-axs.tick_params(axis="both", which="major", labelsize=12)
-# axs.set_ylim(0.8, None)
-axs.set_xlabel("Epoch", fontsize=16)
-axs.set_ylabel("Accuracy", fontsize=16)
-axs.grid()
-axs.legend(fontsize=16, framealpha=1.0)
-
+axs[0].set_ylabel("Accuracy", fontsize=16)
+axs[0].legend(fontsize=16, framealpha=1.0)
 fig.tight_layout()
-plt.savefig(f"{splitext(basename(inspect.stack()[0][1]))[0]}_{split}_{col}.png")
+plt.savefig(f"{splitext(basename(inspect.stack()[0][1]))[0]}_{col}.png")
